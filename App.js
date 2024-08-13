a36ec8d4caaa4d02e1e8d8b9e01e424392d132fc
ry-anzz <rv95025@gmail.com>
2024-08-12 22:53:53 -0300

    'pronto'

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
+
+import React,{useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView, ImageBackground } from 'react-native';
+import backgroundImg from './assets/fundo.jpg';
+
+const request = async(callback) =>{
+  const response = await fetch('https://swapi.dev/api/starships/?page=2');
+  const parsed = await response.json();
+  callback(parsed.results);
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  
+
+  const [registros, setRegistros]= useState([]);
+
+  useEffect(()=>{
+    request(setRegistros);
+  },[]);
+  
+  return(
+   
+
+    
+       <ImageBackground source={backgroundImg} style={styles.fundo}>
+        <Text style={styles.titulo}>API Star Wars</Text>
+        <FlatList
+          data={registros}
+          keyExtractor={(item)=>item.name.toString()}
+          renderItem={({item})=>
+          <Text style= {styles.api}>
+          <Text>Nome: {item.name} {'\n'} {'\n'}</Text>
+          <Text>Modelo: {item.model}</Text>
+          </Text>
+        }
+        />
+     </ImageBackground>
+
+    
   );
+
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+
+api:{
+  backgroundColor:'grey',
+  color: '#00FFFF',
+  padding: 40,
+  borderRadius: 30,
+  marginTop: 40,
+  
+},
+
+titulo:{
+  display: 'flex',
+  textAlign: 'center',
+  justifyContent: 'center',
+  marginTop: 30,
+  fontSize: 40,
+  color:'white',
+},
+
 });
